72b4c80c315e99650c8354cb4e5ccf6369741f8a
kapiosk <29824083+kapiosk@users.noreply.github.com>
2021-10-06 19:17:50 +0300

    .

--- app.py
+++ app.py
@@ -6,43 +6,51 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 baseUrl = 'https://linella.md'
+html = urlopen(f'{baseUrl}/en').read()
+soup = BeautifulSoup(html, features = 'html.parser')
+generalCategories = soup.find_all('ul', class_ = 'level2')
 
-html = urlopen(baseUrl+'/en').read()
-# html = html.decode('utf-8')
-
-soup = BeautifulSoup(html, features='html.parser')
-generalCategories = soup.find_all('ul', class_='level2')
-
-header = ['category', 'page', 'name', 'price']
+header = ['category', 'page', 'name', 'price', 'normal_price']
 
 with open('items.csv', 'w', encoding='UTF8') as f:
     writer = csv.writer(f)
     writer.writerow(header)
     for gc in generalCategories:
-        categories = gc.find_all('a', href=True)
+        categories = gc.find_all('a', href = True)
         for category in categories:
-            hasItems = True
+            hasMorePages = True
             page = 1
-            #while hasItems:
-            href = category['href']
-            catUrl = f'{baseUrl}{href}?page={page}'
-            print(catUrl)
-            try:
-                cathtml = urlopen(catUrl).read()
-                # cathtml = cathtml.decode('utf-8')
-                catSoup = BeautifulSoup(cathtml, features='html.parser')
-                catName = catSoup.find('h1', class_='search-title').text.strip()
-                items = catSoup.find_all('div', class_='tovar__item')
-                for item in items:
-                    if 'slick-slide' not in item.attrs['class']:
-                        price = item.find('span', class_='price__real').text.strip()
-                        name = item.find('h3', class_='tovar__name').text.strip()
-                        if name:
-                            writer.writerow([catName, page, name, price])
-                        #hasItems = False
-                #hasItems = False #not hasItems
-                page += 1
-            except:
-                print("An exception occurred")
-
+            while hasMorePages:
+                href = category['href']
+                catUrl = f'{baseUrl}{href}?page={page}'
+                print(catUrl)
+                try:
+                    cathtml = urlopen(catUrl).read()
+                    catSoup = BeautifulSoup(cathtml, features = 'html.parser')
+                    catName = catSoup.find('h1', class_ = 'search-title').text.strip()
+                    items = catSoup.find_all('div', class_ = 'tovar__item')
+                    for item in items:
+                        if 'slick-slide' not in item.attrs['class']:
+                            price = item.find('span', class_ = 'price__real').text.strip()
+                            np = item.find('span', class_ = 'price__past')
+                            if np:
+                                np = np.text.strip()
+                            else:
+                                np = price
+                            name = item.find('h3', class_ = 'tovar__name').text.strip()
+                            if name:
+                                writer.writerow([catName, page, name, price, np])
+                    paging = catSoup.find_all('a', class_ = 'pag__item', href = True)
+                    if paging:
+                        lastPage = paging[-1].find('span').text
+                        if lastPage.isdigit():
+                            hasMorePages = int(lastPage) > page
+                        else:
+                            hasMorePages = False
+                    else:
+                        hasMorePages = False
+                    page += 1
+                except:
+                    hasMorePages = False
+                    print("An exception occurred")
                 
